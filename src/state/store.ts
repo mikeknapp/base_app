461394aaed27d5f8443f2dd5ddf4878e119c0eb7
Mike Knapp <micknapp@gmail.com>
2023-09-23 17:45:14 +1000

    Zustand and prettier

---
+++ src/state/store.ts
@@ -0,0 +1,37 @@
+import { unstable_batchedUpdates } from "react-dom"
+import { create } from "zustand"
+import { createJSONStorage, devtools, persist } from "zustand/middleware"
+
+export interface AppState {
+  loggedIn: boolean
+  setLoggedIn: (loggedIn: boolean) => void
+
+  clearStore: () => void
+}
+
+export const useAppStore = create<AppState>()(
+  devtools(
+    persist(
+      (set) => ({
+        loggedIn: false,
+        setLoggedIn: (loggedIn: boolean) => set(() => ({ loggedIn: loggedIn })),
+
+        clearStore: () => {
+          set(() => ({
+            loggedIn: false,
+          }))
+        },
+      }),
+      {
+        name: "app-storage",
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+  ),
+)
+
+export const nonReactClearStore = () => {
+  unstable_batchedUpdates(() => {
+    useAppStore.getState().clearStore()
+  })
+}
